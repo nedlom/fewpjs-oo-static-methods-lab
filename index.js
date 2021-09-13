935fea015df7e50494170556b1b91cf4da67ff9b
Nick <nelom3@gmail.com>
2021-09-13 09:00:22 -0700

    commit tree 20def6098f5e951b634d11bd95c7535c1118f2e5

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  static sanitize(str) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  };
+
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const splitStr = str.split(" ");
+    return splitStr.map(function(e, i) {
+      if(i == 0 || !exceptions.includes(e)) {
+        return this.capitalize(e);
+      } else {
+        return e;
+      }
+    }, this).join(" ")
+  }
 }
+
+
+
